873dd3dffaa21636c070b960036fbf6e110af433
Robert <robertly53@gmail.com>
2023-08-04 17:21:07 -0400

    Added firstName, lastName

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -87,6 +87,8 @@ router.get("/", (req, res) => {
       id: user.id,
       email: user.email,
       username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
     };
     return res.json({
       user: safeUser,
